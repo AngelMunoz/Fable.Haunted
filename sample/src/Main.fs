97009a5c3930ce6776f32ac52959bdaa65e7add9
Angel D. Munoz <scythes.lol@gmail.com>
2021-08-27 22:53:51 -0500

    update sample and readme

--- sample/src/Main.fs
+++ sample/src/Main.fs
@@ -4,7 +4,9 @@ open Browser.Types
 open Lit
 open Haunted
 open Haunted.Types
-open Lit.Haunted
+open Fable.Core
+
+JsInterop.importSideEffects "./styles.css"
 
 type EventTarget with
     member this.Value = (this :?> HTMLInputElement).value
@@ -14,66 +16,71 @@ type Msg =
     | Decrement
     | Reset
 
-let private inner_component
-    (props: {| sample: string
-               property: string option |})
+let private custom_element
+    (props: {| sample: string option
+               complexValues: {| message: string |} option |})
     =
-    html $"""<p>An inner component! {props.sample} - {props.property}</p>"""
+    let value =
+        props.complexValues
+        |> Option.defaultValue {| message = "default message" |}
+
+    let sample = defaultArg props.sample "default sample"
+    html $"<p>A! {sample} - {value.message}</p>"
 
 // defineComponent registers a Custom Element so you don't need to actually
 // call this function inside any component, you can use the component itself
-defineComponent "inner-component" (Haunted.Component(inner_component, {| observedAttributes = [| "sample" |] |}))
-
-/// Virtual Components don't have a tag by themselves
-/// but they allow you to have state in them, think of them as if they were fragments
-/// be mindful that since they don't have a tag if you try to dispatch
-/// an event using `this` it might point to the component where this is called
-/// but in some conditons fable will emit an arrow function so this will not be attached
-/// and your dicpatch might fail
-[<VirtualComponent>]
-let virtual_component (name: string) (age: int) =
-    let state, setState = Haunted.useState "I'm so virtual!"
-
-    html
-        $"""
-        <div>
-            <p>A virtual component can keep its own state</p>
-            <input value={state} @keyup={fun (ev: Event) -> ev.target.Value |> setState}>
-            <p>External message: {name} is {age} year(s) old</p>
-            <p>Internal message: {state}</p>
-        </div>"""
+defineComponent
+    "inner-component"
+    (Haunted.Component(
+        custom_element,
+        // if you want to monitor attributes you need to pass an array of attribute names
+        // these will become available in the function's first argument
+        // also these components can be simple custom elements without shadow DOM
+        // if you choose not to use shadow DOM you can use normal css stylesheets like bulma or bootstrap
+        {| observedAttributes = [| "sample" |]
+           useShadowDOM = false |}
+    ))
 
 // by itself lit-html functions are stateless
-let private not_a_component () =
-    html $"""<div>A standard Lit Template!</div>"""
+let private aStatelessFunction paramA paramB =
+    html
+        $"""
+        <div>A standard stateless Lit Template!</div>
+        this will re-render when the parameters change: {paramA} - {paramB}
+        """
+
+let private elmishLike: Reducer<int, Msg> =
+    fun state action ->
+        match action with
+        | Increment -> state + 1
+        | Decrement -> state - 1
+        | Reset -> 0
 
 // we can use haunted to add state to our components
 let private app () =
-    let reducer: Reducer<int, Msg> =
-        fun state action ->
-            match action with
-            | Increment -> state + 1
-            | Decrement -> state - 1
-            | Reset -> 0
 
-    let state, dispatch = Haunted.useReducer (reducer, 0)
+    let state, dispatch = Haunted.useReducer (elmishLike, 0)
 
     let log =
         Haunted.useCallback ((fun x -> printfn "%s" x), [| state |])
 
     log $"{state}"
 
+    let complex =
+        {| message = $"Complex object message value: {state}" |}
+
     html
         $"""
         <h1>Hello, World!</h1>
         <!--You can observe attributes or even properties thanks to lit's templating engine -->
-        <inner-component sample="lol" .property="{10}"></inner-component>
-        {virtual_component "John" 45}
-        {not_a_component ()}
-        <p>Counter: {state}</p>
-        <button @click="{fun _ -> dispatch Increment}">Increment</button>
-        <button @click="{fun _ -> dispatch Decrement}">Decrement</button>
-        <button @click="{fun _ -> dispatch Reset}">Reset</button>
+        <inner-component sample={$"Attribute value: {state}"} .complexValues={complex}></inner-component>
+        {aStatelessFunction "value" state}
+        <section>
+            <p>Counter: {state}</p>
+            <button @click="{fun _ -> dispatch Increment}">Increment</button>
+            <button @click="{fun _ -> dispatch Decrement}">Decrement</button>
+            <button @click="{fun _ -> dispatch Reset}">Reset</button>
+        </section>
         """
 
-defineComponent "fable-app" (Haunted.Component app)
+defineComponent "fable-app" (Haunted.Component(app, {| useShadowDOM = false |}))
